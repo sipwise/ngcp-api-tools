9d424a21d1136cac9c96925d0d34db1c310770c3
Flaviu Mates <fmates@sipwise.com>
2019-03-20 19:13:56 +0200

    TT#53575 Fix next page method
    	* use URI in order to remove doubling of query parameters
    	  because they were seen as array refs
    
    Change-Id: I73e25860381d4dcba0197b40d603a6d4933b6b84

--- lib/NGCP/API/Client.pm
+++ lib/NGCP/API/Client.pm
@@ -8,6 +8,8 @@ use IO::Socket::SSL;
 use LWP::UserAgent;
 use Readonly;
 use Encode;
+use URI;
+use Data::Dumper;
 
 my $config;
 
@@ -104,11 +106,7 @@ sub request {
 }
 
 sub next_page {
-    my ($self, $uri) = @_;
-
-    (my $params = $uri) =~ s/^[^?]+\?//;
-    $params =~ s/[&?]rows(=\d+)?//;
-    $params =~ s/[&?]page(=\d+)?//;
+    my ( $self, $uri ) = @_;
 
     unless ($self->{_ua}) {
         ($self->{_ua},$self->{_urlbase}) = $self->_create_ua($uri);
@@ -130,7 +128,9 @@ sub next_page {
     if ($data && ref($data) eq 'HASH') {
         $uri = $data->{_links}->{next}->{href};
         return $res unless $uri && $uri =~ /page/;
-        $uri .= '&'.$params if $params && $uri !~ /\Q$params\E/;
+        my %params = $uri =~ /([\d\w\s]+)=([^&]+)&*/g;
+        $uri = URI->new($uri);
+        $uri->query_form( \%params );
         $self->{_collection_url} = $self->_get_url($self->{_urlbase},$uri) if $uri;
     }
 
