89b052600c980559365831453145a6d276a65083
Guillem Jover <gjover@sipwise.com>
2019-10-09 19:04:47 +0200

    TT#68300 Fix LICENSE and COPYRIGHT information
    Update copyright years and change license to match the one declared in
    debian/copyright, and the other perl programs in the repo.
    
    Change-Id: I8d2cb1d38f267bea8c4f399ce4b921a260e2e862

--- lib/NGCP/API/Client.pm
+++ lib/NGCP/API/Client.pm
@@ -404,11 +404,20 @@ Kirill Solomko <ksolomko@sipwise.com>
 
 =head1 LICENSE
 
-This software is Copyright (c) 2016 by Sipwise GmbH, Austria.
+Copyright (c) 2016-2019 Sipwise GmbH, Austria.
 
-All rights reserved. You may not copy, distribute
-or modify without prior written permission from
-Sipwise GmbH, Austria.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 =cut
 
