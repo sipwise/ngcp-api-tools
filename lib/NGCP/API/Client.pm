sipwise/ngcp-api-tools

commit 97c0fb6412226de3f02f40a17a9aa783e2449b3e

TT#68300 Load the configuration file from the constructor
We should not be running code that loads a configuration file in
a BEGIN block, as that will execute just by loading the module.

This makes using this module conditionally in other programs or testing
that code more difficult, as we would need to mock either this module
or the configuration file.

Just load the configuration file once in the constructor and cache
its contents, to avoid the overhead.

Change-Id: I86986dc81eaa32833b738ffa6df2a5de30159482

lib/NGCP/API/Client.pm
@@ -1,6 +1,8 @@
 package NGCP::API::Client;
 use strict;
 use warnings;
+use feature qw(state);
+
 use English qw(-no_match_vars);
 use Config::Tiny;
 use JSON::XS;
@@ -9,15 +11,34 @@ use LWP::UserAgent;
 use Readonly;
 use URI;
 
-my $config;
+sub _load_config_defaults {
+    my $cfg_file = '/etc/default/ngcp-api';
+    my $cfg;
 
-BEGIN {
-    my $cfg_file = "/etc/default/ngcp-api";
-    $config = Config::Tiny->read($cfg_file)
-                        or die "Cannot read $cfg_file: $ERRNO";
-};
+    $cfg = Config::Tiny->read($cfg_file)
+        or die "Cannot read $cfg_file: $ERRNO";
 
-Readonly::Scalar my $cfg => $config;
+    Readonly my $config => {
+        host          => $cfg->{_}->{NGCP_API_IP},
+        port          => $cfg->{_}->{NGCP_API_PORT},
+        iface         => $cfg->{_}->{NGCP_API_IFACE},
+        sslverify     => $cfg->{_}->{NGCP_API_SSLVERIFY} || 'yes',
+        sslverify_lb  => $cfg->{_}->{NGCP_API_SSLVERIFY_LOOPBACK} || 'no',
+        read_timeout  => $cfg->{_}->{NGCP_API_READ_TIMEOUT} || 180,
+        page_rows     => $cfg->{_}->{NGCP_API_PAGE_ROWS} // 10,
+        auth_user     => $cfg->{_}->{AUTH_SYSTEM_LOGIN},
+        auth_pass     => $cfg->{_}->{AUTH_SYSTEM_PASSWORD},
+        verbose       => 0,
+    };
+
+    return $config;
+}
+
+sub _get_config_defaults {
+    state $config = _load_config_defaults();
+
+    return $config;
+}
 
 my %opts = ();
 
@@ -83,18 +104,12 @@ sub new {
     my $class  = shift;
     my $self   = {};
 
-    $opts{host}         = $cfg->{_}->{NGCP_API_IP};
-    $opts{port}         = $cfg->{_}->{NGCP_API_PORT};
-    $opts{iface}        = $cfg->{_}->{NGCP_API_IFACE};
-    $opts{sslverify}    = $cfg->{_}->{NGCP_API_SSLVERIFY} || 'yes';
-    $opts{sslverify_lb} = $cfg->{_}->{NGCP_API_SSLVERIFY_LOOPBACK} || 'no';
-    $opts{read_timeout} = $cfg->{_}->{NGCP_API_READ_TIMEOUT} || 180;
-    $opts{auth_user}    = $cfg->{_}->{AUTH_SYSTEM_LOGIN};
-    $opts{auth_pass}    = $cfg->{_}->{AUTH_SYSTEM_PASSWORD};
-    $opts{verbose}      = 0;
-
     bless $self, $class;
-    $self->set_page_rows($cfg->{_}->{NGCP_API_PAGE_ROWS} // 10);
+
+    my $default_opts = _get_config_defaults();
+    foreach my $opt (keys %{$default_opts}) {
+        $opts{$opt} //= $default_opts->{$opt};
+    }
     $self->_create_ua();
 
     return $self;

commit d5e57595d4f90365c498254ead413b86f3af8887

TT#68300 NGCP::API::Client: Get the page rows from the _opts hash
When moving to per-object options, we switched to set the page rows
directly from the constructor, and switched from the _rows to
_opts.page_rows.

Change-Id: Ia663dbba4a330813d67f6bbdbb50ad37419ad5d5
Fixes: commit 97c0fb6412226de3f02f40a17a9aa783e2449b3e

lib/NGCP/API/Client.pm
@@ -146,7 +146,7 @@ sub next_page {
 
         my %params = $collection_url->query_form;
         $params{page} //= 1;
-        $params{rows} //= $self->{_rows};
+        $params{rows} //= $self->{_opts}{page_rows};
         $collection_url->query_form(\%params);
 
         $self->{_collection_url} = $collection_url;
@@ -176,7 +176,7 @@ sub next_page {
 sub set_page_rows {
     my ($self, $rows) = @_;
 
-    $self->{_rows} = $rows;
+    $self->{_opts}{page_rows} = $rows;
     undef $self->{_collection_url};
 
     return;